344722110e3dd506692111a2cb8c9390dcc12f51
Hasan Mehdi Rizvi <hasanmrizvi72@gmail.com>
2024-03-01 10:00:00 +0530

    Initial project setup: configs and tooling

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.(test|spec).(ts|tsx|js)'
+  ],
+};
